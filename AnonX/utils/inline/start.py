30e9955455ece707c36367cd843d6e3efdfcf56b
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-07-30 15:30:46 +0530

    Update start.py
    Signed-off-by: SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -1,26 +1,21 @@
 from typing import Union
-
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-import config
+from SankiMusic.utilities.config import config
 
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                text="≽ ᴄᴏᴍᴍᴀɴᴅs ≼",
+                url=f"https://t.me/{BOT_USERNAME}?start=help",
             )
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ",
-                callback_data="settings_back_helper",
-            ),
-            InlineKeyboardButton(
-                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
-            ),
+                text="✮ sᴇᴛᴛɪɴɢs ✮", callback_data="settings_helper"
+            )
         ],
      ]
     return buttons
@@ -36,21 +31,11 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
+                text="✭ ᴜᴘᴅᴀᴛᴇs ✭", url=config.SUPPORT_CHANNEL
             ),
             InlineKeyboardButton(
-                text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
+                text="✭ sᴜᴘᴘᴏʀᴛ ✭", url=config.SUPPORT_GROUP
             )
-        ],
-        [
-            InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
-            )
-        ],
+        ]
      ]
     return buttons
