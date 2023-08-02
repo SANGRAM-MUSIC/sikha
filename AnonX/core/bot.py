7b4fe02122abd2fb6989c3321cc41376fdf6b322
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-08-02 07:00:04 +0530

    Update bot.py
    Signed-off-by: SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -33,12 +33,12 @@ class AnonXBot(Client):
             LOGGER(__name__).error(
                 "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
-            sys.exit()
+            
 
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
                 "Please promote Bot as Admin in Logger Group"
             )
-            sys.exit()
+            
         LOGGER(__name__).info(f"MusicBot Started as @{self.username}")
